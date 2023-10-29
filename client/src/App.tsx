528d76069122e646c9187bdaf1c67d14775db525
Freddo <fredrik.gullin@gmail.com>
2023-10-29 03:40:46 +0100

    added navbar-footer-home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./components/footer/footer";
 import NavScroll from "./components/navbar/navbar";
+import Home from "./views/home/home";
 
 function App() {
   return (
     <>
       <NavScroll />
+      <Home />
+      <Footer />
     </>
   );
 }
